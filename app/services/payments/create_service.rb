09ddff80b976d88fa8a6f43c31dd1c141f6537a5
toan_au <toanau.dev@gmail.com>
2026-04-20 11:47:39 +0900

    fix: added merchant specific security in creating payments

--- app/services/payments/create_service.rb
+++ app/services/payments/create_service.rb
@@ -14,10 +14,10 @@ module Payments
     def call
       raise PaymentError::InvalidCurrency unless Money::Currency.find(@params[:currency])
 
-      existing = Transaction.find_by(idempotency_key: @params[:idempotency_key])
+      existing = @merchant.transactions.find_by(idempotency_key: @params[:idempotency_key])
       return Result.new(transaction: existing, status: :ok) if existing
 
-      transaction = Transaction.new(@params.merge(merchant: @merchant))
+      transaction = @merchant.transactions.new(@params)
 
       raise PaymentError::ValidationFailed, transaction.errors.full_messages unless transaction.save
 
