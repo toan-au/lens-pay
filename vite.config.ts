5b564d3fe2f5d8bb5dc49ed3413fd3bc40263cfc
toan_au <toanau.dev@gmail.com>
2026-04-30 16:30:48 +0900

    Added Tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import RubyPlugin from "vite-plugin-ruby";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    RubyPlugin(),
-    vue(),
-  ],
-})
+  plugins: [RubyPlugin(), vue(), tailwindcss()],
+});
